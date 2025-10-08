32094f54c214ab30e915134d16fb9f6b5a39a1cd
82481 <82481@RAHUL>
2025-10-08 15:13:42 +0530

    exception trycatch

---
+++ tnsif_java_rahulgandhi/src/tnsif_java_rahulgandhi/TryCatch.java
@@ -0,0 +1,37 @@
+package tnsif_java_rahulgandhi;
+
+
+public class TryCatch {
+	public static void main(String[] args) {
+		System.out.println("I am in main method");
+		int result;
+		try {
+
+			result = TryCatch.performDivision(12, 0);
+			if (result != 0)
+				System.out.println("Division is " + result);
+
+			System.out.println("------------------------------");
+
+			result = TryCatch.performDivision(12, 3);
+			if (result != 0)
+				System.out.println("Division is " + result);
+
+			System.out.println("------------------------------");
+		} catch (ArithmeticException e) {
+			//System.out.println("I am in catch block: " + e.getMessage());
+			e.printStackTrace();
+		}
+		System.out.println(TryCatch.performDivision(12f, 5f));
+
+		System.out.println("------------------------------");
+		System.out.println(TryCatch.performDivision(12f, 0f));
+
+	}
+
+	private static int performDivision(float f, float g) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
