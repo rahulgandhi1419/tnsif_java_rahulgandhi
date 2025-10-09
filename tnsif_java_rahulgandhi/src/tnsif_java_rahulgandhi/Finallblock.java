5b5ab8979a92be96ceadc5f4c31e98f887e9e488
82481 <82481@RAHUL>
2025-10-09 14:19:22 +0530

    finallblock

---
+++ tnsif_java_rahulgandhi/src/tnsif_java_rahulgandhi/Finallblock.java
@@ -0,0 +1,20 @@
+package tnsif_java_rahulgandhi;
+
+
+public class FinallBlock {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		try {
+			System.out.print("Enter the number : ");
+			int number = scanner.nextInt();
+			System.out.println(number % 2 == 0 ? number + " is Even" : number + " is Odd");
+			return;
+		} catch (InputMismatchException e) {
+			System.err.println("Invalid input...");
+		} finally {
+			System.out.println("In Finally....");
+			scanner.close();
+		}
+	}
+}
