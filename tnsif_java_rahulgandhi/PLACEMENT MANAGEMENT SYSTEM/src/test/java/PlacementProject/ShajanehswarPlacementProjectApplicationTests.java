469f52173b0c87ff911902b9897246e634dbaca6
rahulgandhi1419 <rahulgandhi1419@gmail.com>
2025-11-12 19:48:48 +0530

    Upload Spring Boot project files

---
+++ tnsif_java_rahulgandhi/PLACEMENT MANAGEMENT SYSTEM/src/test/java/PlacementProject/ShajanehswarPlacementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package PlacementProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShajanehswarPlacementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
